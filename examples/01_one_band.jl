a34a893e09139c183ac64f422e7b49f8649536dc
qstaelens <quentin.staelens@outlook.com>
2025-12-08 16:58:16 +0100

    Change one_band example

--- examples/01_one_band.jl
+++ examples/01_one_band.jl
@@ -1,5 +1,5 @@
 using HubbardTN
-using TensorKit, MPSKit
+using TensorKit, MPSKit, Statistics
 
 # Step 1: Define the symmetries
 particle_symmetry = U1Irrep
@@ -21,9 +21,21 @@ gs = compute_groundstate(calc)
 ψ = gs["groundstate"]
 H = gs["ham"]
 
-println("Ground-state energy density: ", expectation_value(ψ, H) / length(H))
+E0 = expectation_value(ψ, H)
+E = sum(real(E0)) / length(H)
+println("Groundstate energy: $E")
 
-# Step 4: Compute first excitation in fZ2(0) × U1Irrep(0) × U1Irrep(0) sector
-momenta = collect(range(0, 2π, length = 10))
-charges = [0.0, 0.0, 0.0]
-ex = compute_excitations(gs, momenta, charges)
+dim = dim_state(ψ)
+b = mean(dim)
+println("Mean bond dimension: $b")
+
+e = entanglement_spectrum(ψ)
+println("Entanglement spectrum: $e")
+
+Ne = density_e(ψ,symm)
+println("Ne: $Ne")
+println("Mean Ne = ", mean(Ne))
+
+u, d = density_spin(ψ,symm)
+println("Spin up: $u")
+println("Spin down: $d")
