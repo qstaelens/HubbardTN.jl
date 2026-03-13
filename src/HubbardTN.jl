1c1db621484886485cda1fa01e3491e42ea4f169
qstaelens <quentin.staelens@outlook.com>
2026-03-13 13:44:59 +0100

    Add charge gap

--- src/HubbardTN.jl
+++ src/HubbardTN.jl
@@ -7,7 +7,7 @@ export create_pair_onesite, delete_pair_onesite
 export SymmetryConfig, HubbardParams, CalcConfig
 export ThreeBodyTerm, MagneticField, StaggeredField, HolsteinTerm, Bollmark
 export hamiltonian, compute_groundstate, find_chemical_potential
-export compute_excitations, compute_domainwall, compute_bandgap, compute_spingap, compute_pairing_energy
+export compute_excitations, compute_domainwall, compute_bandgap, compute_spingap, compute_pairing_energy, compute_chargegap
 export dim_state, density_e, density_b, density_spin, calc_ms, density_correlations
 export save_computation, load_computation, save_state, load_state, dict_tag
 export get_alpha, get_beta
